3e07f5abb2a4321d071c4e396d8f2d8c6ea4997d
carojane <carojane@gmail.com>
2014-09-17 18:47:14 -0400

    add new question, see all questions with links

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,28 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all.order('created_at DESC')
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :description)
+    end
+end
