0f0ca1caa3ca889cfe9d4c15c3af4cf5ab9347fe
carojane <carojane@gmail.com>
2014-09-17 20:39:10 -0400

    typo in validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
 
-  validate :description, presence: true, length: { minimum: 50 }
+  validates :description, presence: true, length: { minimum: 50 }
 end
