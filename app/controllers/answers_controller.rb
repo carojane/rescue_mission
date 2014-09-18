d8a43c4668a0b25255da6371a164f6a7fdaa8b72
carojane <carojane@gmail.com>
2014-09-18 13:39:52 -0400

    select best answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,6 +8,18 @@ class AnswersController < ApplicationController
       redirect_to question_path(@question)
   end
 
+  def edit
+    @question = Question.find(params[:question_id])
+
+    @best_answer = Answer.find_by(best: true)
+    @best_answer.update_attribute(:best, false)
+
+    @answer = Answer.find(params[:id])
+    @answer.update_attribute(:best, true)
+
+    redirect_to @question
+  end
+
   private
     def answer_params
       params.require(:answer).permit(:description)
--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,7 +1,3 @@
 module AnswersHelper
-  def choose_best(answer)
-    @best_answer = Answer.find_by(best: true)
-    @best_answer.update(best: false)
-    answer.update(best: true)
-  end
+
 end
