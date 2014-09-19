f2e44579e9dce886941e2bfd9ff1260a34217665
carojane <carojane@gmail.com>
2014-09-19 15:59:11 -0400

    omniauth sign in with github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
 
 def create
-  user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+  user = User.find_by(provider: auth["provider"], uid: auth["uid"]) || User.create_with_omniauth(auth)
   session[:user_id] = user.id
-  redirect_to '/', notice: "Signed in!"
+  redirect_to '/questions', notice: "Signed in!"
 end
 
 def destroy
   session[:user_id] = nil
-  redirect_to root_url, notice: "Signed out!"
+  redirect_to '/', notice: "Signed out!"
 end
 
 protected
